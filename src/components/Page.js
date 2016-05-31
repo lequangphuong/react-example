ac653f85b2c9702e860363b9c9c7253315e154a9
Lê Quang Phương <quangphuongle1992@gmail.com>
2016-06-01 00:06:42 +0700

    example reactjs

---
+++ src/components/Page.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+class Page extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>Page 1</div>
+
+    );
+  }
+}
+
+module.exports = Page;
---
+++ src/main.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Page = require('./components/Page');
+
+ReactDOM.render(
+  <Page />,
+  document.getElementById('main')
+);
